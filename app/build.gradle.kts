1fed455ec660cb22ccf8a8211ad95163c13fe4b0
Sailesh Kushwaha <saileshkushwaha@users.noreply.github.com>
2026-09-16 21:42:20 +0530

    fix: exclude duplicate JGit resources from merge
    JGit JARs contain duplicate OSGI-INF and META-INF files that cause
    mergeDebugJavaResource failures. Added packaging excludes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,12 @@ android {
     packaging {
       resources {
         excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        excludes += "OSGI-INF/l10n/plugin.properties"
+        excludes += "META-INF/DEPENDENCIES"
+        excludes += "META-INF/LICENSE"
+        excludes += "META-INF/LICENSE.txt"
+        excludes += "META-INF/NOTICE"
+        excludes += "META-INF/NOTICE.txt"
       }
     }
 
